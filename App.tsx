9208b91dd5fae6aca37aba560a7398df2c32f940
Tsz-Ching Hon <69753094+JudyHon@users.noreply.github.com>
2025-06-23 11:54:01 +0200

    set up for EAS Build

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
 import TodoApp from "./route/TodoApp/TodoApp";
+import { COLORS } from "./utils/theme";
 
 export default function App() {
   return (
@@ -14,5 +15,6 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
+    backgroundColor: COLORS.white,
   },
 });
